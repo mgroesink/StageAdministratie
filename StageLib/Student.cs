5d4c5f37ffe80b2d84e5a899d854b35f66f39aae
mgroesink <mroesink@rocvantwente.nl>
2022-02-09 15:21:21 +0100

    Add project files.

---
+++ StageLib/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace StageLib
+{
+    public class Student
+    {
+        private string _studentnummer;
+        private string _voornaam;
+        private string _achternaam;
+        private DateTime _geboortedatum;
+
+        public Student(string studentnummer,string voornaam , string achternaam)
+        { 
+            _studentnummer = studentnummer;
+            _voornaam = voornaam;   
+            _achternaam = achternaam;
+        }
+        public Student()
+        {
+
+        }
+
+        public string Voornaam
+        {
+            get { return _voornaam; }
+        }
+    }
+}
---
+++ StageMVC/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StageLib;
+
+namespace StageMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var s1 = new Student("rsk01", "Marcel", "Roesink");
+            var s2 = new Student();
+            var s = s1.Voornaam;
+            return View();
+        }
+    }
+}
